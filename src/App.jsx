e6ceb9429cc36f4315f68fbfc7918afec2c719f3
ameligit <its.me.ameli@proton.me>
2024-06-28 21:06:47 +0330

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { AppProvider, Card, Page, BlockStack, Text } from '@shopify/polaris';
+import '@shopify/polaris/build/esm/styles.css';
+
+function App() {
+  return (
+    <AppProvider>
+      <Page title="Example">
+        <Card sectioned>
+          <BlockStack>
+            <Text as="p">
+              Polaris is a design system that helps us build a great experience for all of our merchants.
+            </Text>
+          </BlockStack>
+        </Card>
+      </Page>
+    </AppProvider>
+  );
+}
+
+export default App;
