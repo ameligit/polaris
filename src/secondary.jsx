4623296fc3b9365a930bc28061194925502ed6c9
ameligit <its.me.ameli@proton.me>
2024-06-30 11:44:31 +0330

    Update Polaris styles import path and Cloudflare settings

--- src/secondary.jsx
+++ src/secondary.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { AppProvider, Page, Card, Text } from '@shopify/polaris';
-import '../node_modules/@shopify/polaris/build/esm/styles.css';
-import './index.css'
+import {
+    Page,
+    Layout,
+    Banner,
+    LegacyCard,
+    FormLayout,
+    TextField,
+  } from '@shopify/polaris';
+  import '../node_modules/@shopify/polaris/build/esm/styles.css';
+  import './secondex.css'
+  import React from 'react';
+  import ReactDOM from 'react-dom';
 
-function SecondaryApp() {
-  return (
-    <AppProvider>
-      <Page title="صفحه دوم">
-        <Card sectioned>
-          <Text as="p">این یک صفحه دوم است.</Text>
-        </Card>
+
+  function LayoutExample() {
+    return (
+      <Page fullWidth>
+        <Layout>
+          <Layout.Section>
+            <Banner title="Order archived" onDismiss={() => {}}>
+              <p>This order was archived on March 7, 2017 at 3:12pm EDT.</p>
+            </Banner>
+          </Layout.Section>
+          <Layout.AnnotatedSection
+            id="storeDetails"
+            title="Store details"
+            description="Shopify and your customers will use this information to contact you."
+          >
+            <LegacyCard sectioned>
+              <FormLayout>
+                <TextField
+                  label="Store name"
+                  onChange={() => {}}
+                  autoComplete="off"
+                />
+                <TextField
+                  type="email"
+                  label="Account email"
+                  onChange={() => {}}
+                  autoComplete="email"
+                />
+              </FormLayout>
+            </LegacyCard>
+          </Layout.AnnotatedSection>
+        </Layout>
       </Page>
-    </AppProvider>
-  );
-}
+    );
+  }
 
 ReactDOM.render(<SecondaryApp />, document.getElementById('root-secondary'));
